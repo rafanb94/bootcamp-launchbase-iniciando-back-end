4db075b1fb732423e6ce843a51cdd40bb0e3e4dd
Rafael Brunner <rafael_nbc@hotmail.com>
2020-03-04 02:43:49 -0300

    update

--- server.js
+++ server.js
@@ -2,15 +2,38 @@ const express = require ('express')
 const nunjucks = require('nunjucks')
 const server = express()
 
+server.use(express.static('public'))
 server.set('view engine','html')
 nunjucks.configure('views', {
     express:server
 })
 
-server.get('/', function(request, response){
-return response.send('Hi')
-
+server.get('/', function(request, response)
+{
+return response.render("home")
 })
+server.get('/aulas', function(request, response)
+{
+return response.render("aulas")
+})
+server.get('/cursos', function(request, response)
+{
+return response.render("cursos")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 server.listen(5000, function()
 {
 
