2830bb3d61ef1f806d34624c5746c885ad468b0f
Rafael Brunner <rafael_nbc@hotmail.com>
2020-03-05 02:53:13 -0300

    update

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+    id: "2alg7MQ6_sI",
+    title:"CSS Grid Layout",
+    featured: "true"
+    },
+    {
+        id: "ghTrp1x_1As",
+        title:"CSS Grid Layout 2"
+    },
+    {
+        id: "AVNADGzXrrQ",
+        title:"CSS Grid Layout 3",
+        featured: "true"
+    },
+]
--- server.js
+++ server.js
@@ -1,41 +1,33 @@
-const express = require ('express')
-const nunjucks = require('nunjucks')
-const server = express()
+const express = require("express");
+const nunjucks = require("nunjucks");
 
-server.use(express.static('public'))
-server.set('view engine','html')
-nunjucks.configure('views', {
-    express:server
-})
+const server = express();
+const videos = require("./data");
 
-server.get('/', function(request, response)
-{
-return response.render("home")
-})
-server.get('/aulas', function(request, response)
-{
-return response.render("aulas")
-})
-server.get('/cursos', function(request, response)
-{
-return response.render("cursos")
-})
+server.use(express.static("public"));
+server.set("view engine", "njk");
+nunjucks.configure("views", {
+    express: server
+});
 
+server.get("/", function (request, response) {
+    const home = {
+        avatar_url:"https://scontent.fcgh10-1.fna.fbcdn.net/v/t1.0-1/p160x160/59407476_2184049961702192_6987893004405047296_o.jpg?_nc_cat=108&_nc_ohc=HpQMBOJ28c4AX9BEA-V&_nc_ht=scontent.fcgh10-1.fna&_nc_tp=6&oh=98f9a763de8e57793a88656b05d533f2&oe=5EC7944C",
+        description:'Portófilo Profissional | Rafael Brunner',
+        formation:"Formação em Análise e Desenvolvimento de Sistemas",
+        role:"Consultor SEO por SEO Unlocked",
+        links: [
+            {name: "Github", url:'https://github.com/rafanb94'},
+            {name: "Linkedin", url:'https://www.linkedin.com/in/rafael-nociolino-brunner-9b2102169/'}
+        ]
+    }
+    return response.render("home", {home});
+});
+server.get("/aulas", function (request, response) {
+    return response.render("aulas",{items: videos});
+});
+server.get("/cursos", function (request, response) {
+    return response.render("cursos");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-server.listen(5000, function()
-{
-
-
-})
+server.listen(5000, function () { });
